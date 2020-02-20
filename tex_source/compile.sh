c3c24862e7bdb0b44d682074cfa059d80f13409a
kosugitti <kosugitti@gmail.com>
2020-02-20 15:59:41 +0900

    コンパイル情報も追加

--- tex_source/compile.sh
+++ tex_source/compile.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
-rm *.aux
-rm *.log
-rm *.bbl
-rm *.blg
+cd `dirname $0`
 uplatex syllabus.tex
 upbibtex syllabus
 uplatex syllabus.tex
 uplatex syllabus.tex
 dvipdfmx syllabus.dvi
-
+rm *.aux
+rm *.log
+rm *.dvi
+rm *.toc
+rm *.bbl
+rm *.blg
+mv -f syllabus.pdf ../syllabus.pdf
