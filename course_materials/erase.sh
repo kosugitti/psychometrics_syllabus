4e69363b1fa134cb0ee1f66ec31e87fba1a3c1a9
Koji E. Kosugi <kosugitti@gmail.com>
2020-09-26 07:45:16 +0900

    誤字修正

--- course_materials/erase.sh
+++ course_materials/erase.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
+rm *.pdf
+uplatex -src-special -interaction=nonstopmode -shell-escape textbook.tex
+upbibtex textbook
+uplatex -src-special -interaction=nonstopmode -shell-escape textbook.tex
+dvipdfmx textbook.dvi
 rm *.aux
 rm *.log
 rm *.dvi
