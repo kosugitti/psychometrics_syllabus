5c7a4e674d52181fa10fb302b2470217cf8efe76
kosugitti <kosugitti@gmail.com>
2021-03-24 10:09:37 +0900

    ソースフォルダ名変更

---
+++ course_materials2/R_source_course2/20210323.R
@@ -0,0 +1,21 @@
+rm(list=ls())
+A <- matrix(c(1,2,3,4),ncol=2)
+B <- matrix(c(3,2,1,4,5,6),ncol=3)
+C <- matrix(c(4,2,1,3),ncol=2)
+x <- matrix(c(1,2,3),ncol=1)
+y <- matrix(c(2,8),ncol=2)
+A
+B
+C
+x
+y
+A + B
+A - C
+A %*% B
+A %*% C
+t(B) %*% A
+A %*% t(y)
+x %*% y
+x %*% B
+t(x) %*% t(B)
+y %*% x
