8dc47b6b046988034287a9e99e277ab1718c1111
kosugitti <kosugitti@gmail.com>
2022-04-18 10:10:34 +0900

    0418

--- course_materials2/R_source_course2/20210929.R
+++
@@ -1,9 +0,0 @@
-rm(list = ls())
-library(tidyverse)
-library(bayesplot)
-library(cmdstanr)
-
-wrongModel <-cmdstan_model("wrong.stan")
-fit <- wrongModel$sample()
-g <- fit$draws("a") %>% bayesplot::mcmc_trace()
-ggsave(g,filename = "../images/chapter18/Rplot18_02.png", dpi = 600, width = 8, height = 4)
