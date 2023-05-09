ae2de91c779f505e42fe69df19f777099ab6fde0
kosugitti <kosugitti@gmail.com>
2023-05-09 09:49:47 +0900

    0502

--- LABC/LABC_Rproj/20230502.R
+++ LABC/LABC_Rproj/20230502.R
@@ -6,17 +6,30 @@ G <- c(1,1,1,1)
 PG <- G %*%solve((t(G)%*%G))%*%t(G)
 PF <- PA - PG
 
-Y%*%PA
-Y%*%PE
-Y%*%PG
-Y%*%PF
+PA%*%Y
+PE%*%Y
+PG%*%Y
+PF%*%Y
 I <- PG+PF+PE
 
-t(Y) %*% I %*% Y
-t(Y) %*% PG %*% Y
-t(Y) %*% PF %*% Y
-t(Y) %*% PE %*% Y
+y <- Y - mean(Y)
+
+t(y) %*% I %*% y
+t(y) %*% PG %*% y
+t(y) %*% PF %*% y
+t(y) %*% PE %*% y
 
 sum(diag(PE))
 sum(diag(PF))
 sum(diag(PG))
+
+# 切片を含む -------------------------------------------------------------------
+
+## ランク落ち
+X <- matrix(c(1,1,1,1,1,1,0,0,0,0,1,1),ncol=3)
+PA <- X %*%solve((t(X)%*%X))%*%t(X)
+
+## ランク対応
+X <- matrix(c(1,1,1,1,1,1,-1,-1),ncol=2)
+PA <- X %*%solve((t(X)%*%X))%*%t(X)
+Y%*%PA
